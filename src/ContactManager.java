a245ab11a4e2841062a5c74545ab61a64e1abac1
Eoors <eors.mezey@gmail.com>
2018-10-10 16:53:18 +0200

    Upload the whole project

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactManager {
+
+    List<Contact> myFriends;
+    int friendsCount;
+
+    ContactManager()
+    {
+        this.friendsCount = 0;
+        List<Contact> myFriends = new ArrayList<>();
+
+    }
+
+    void addContact(Contact contact)
+    {
+        myFriends.set(friendsCount, contact);
+        friendsCount++;
+    }
+
+    Contact searchContact(String searchName)
+    {
+        for(int i = 0; i < friendsCount ; i++)
+        {
+            if(myFriends.get(i).name.equals(searchName))
+            {
+                return myFriends.get(i);
+            }
+        }
+        return null;
+    }
 }
